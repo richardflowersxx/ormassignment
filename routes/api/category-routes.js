c8c440d85f3f1dcca1b03a13cbd72018d184fd4b
Ricardo Flores <richard.flowersxx@gmail.com>
2021-07-13 17:30:05 -0400

    finish all routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,14 +27,34 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then(function(data){
+    console.log(data)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body,{
+    where:{
+      id:req.params.id
+    }
+  })
+  .then(function(data){
+    console.log(data)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where:{
+      id:req.params.id
+    }
+  })
+  .then(function(data){
+    console.log(data)
+  })
 });
 
 module.exports = router;
