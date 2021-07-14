74aa94d7f1dc05f463eb236177213cb1138d2503
Ricardo Flores <richard.flowersxx@gmail.com>
2021-07-14 18:36:34 -0400

    routes and models updated

--- models/Category.js
+++ models/Category.js
@@ -6,13 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
-    id:{
+    id: {
       type: DataTypes.INTEGER,
-      allowNull:false,
-      primaryKey:true,
-      autoIncrement:true
-    }
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
--- models/Tag.js
+++ models/Tag.js
@@ -6,16 +6,15 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
-    tag_name:{
-      type: DataTypes.STRING, allowNull: false 
-  },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
